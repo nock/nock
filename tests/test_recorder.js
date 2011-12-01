6923a5a8f28d02a46446da214e34d503d561297a
Pedro Teixeira <pedro.teixeira@gmail.com>
2011-12-01 19:36:27 +0000

    corrected generator

--- tests/test_recorder.js
+++ tests/test_recorder.js
@@ -19,7 +19,7 @@ tap.test('records', function(t) {
       nock.restore();
       ret = nock.recorder.play();
       t.equal(ret.length, 1);
-      t.equal(ret[0].indexOf("\nnock('expensecat.iriscouch.com')\n  .post('/, \"ABCDEF\"')\n  reply.("), 0);
+      t.equal(ret[0].indexOf("\nnock('expensecat.iriscouch.com')\n  .post('/, \"ABCDEF\"')\n  .reply("), 0);
       t.end();
     });
   });
