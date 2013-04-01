ebed85697121a9e0273422f700d377b4000fbd3a
Pedro Teixeira <pedro.teixeira@gmail.com>
2013-04-01 15:52:08 +0100

    resuming response on recorder test

--- tests/test_recorder.js
+++ tests/test_recorder.js
@@ -6,26 +6,28 @@ tap.test('records', function(t) {
   nock.restore();
   var cb1 = false
     , options = { method: 'POST'
-                , host:'expensecat.iriscouch.com'
+                , host:'google.com'
                 , port:80
                 , path:'/' }
   ;
 
   nock.recorder.rec(true);
   var req = http.request(options, function(res) {
+    res.resume();
     cb1 = true
     var ret;
     res.once('end', function() {
       nock.restore();
       ret = nock.recorder.play();
       t.equal(ret.length, 1);
-      t.equal(ret[0].indexOf("\nnock('http://expensecat.iriscouch.com')\n  .post('/', \"ABCDEF\")\n  .reply("), 0);
+      t.equal(ret[0].indexOf("\nnock('http://google.com')\n  .post('/', \"ABCDEF\")\n  .reply("), 0);
       t.end();
     });
   });
   req.end('ABCDEF');
   return req;
 });
+
 
 tap.test('checks if callback is specified', function(t) {
   var options = {
