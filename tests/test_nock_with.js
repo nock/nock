5e38823040629047d71c80fbea2148d0bf07c414
Keith Laban <kelaban17@gmail.com>
2014-10-10 15:08:46 -0400

    added new nockWith modes and fixed tests: *WARNING* only disabled and record mode are tested so far

--- tests/test_nock_with.js
+++ tests/test_nock_with.js
@@ -5,97 +5,105 @@ var nockWith = require('../.').nockWith
   , fs      = require('fs')
   , exists  = fs.existsSync;
 
+tap.test('nockWith tests', function (nw) {
+  nockWith.setMode('record');
 
-tap.test('nockWith throw an execption when fixtures is not set', function (t) {
+  nw.test('nockWith throw an execption when fixtures is not set', function (t) {
 
-  try {
-    nockWith();
-  } catch (e) {
-    t.ok(true, 'excpected exception');
-    t.end();
-    return;
-  }
+    try {
+      nockWith();
+    } catch (e) {
+      t.ok(true, 'excpected exception');
+      t.end();
+      return;
+    }
 
-  t.fail(true, false, 'test should have ended');
+    t.fail(true, false, 'test should have ended');
 
-});
-
-tap.test('it records when configured correctly', function (t) {
-  nockWith.fixtures = __dirname + '/fixtures';
-
-  var options = {
-    host: 'www.google.com', method: 'GET', path: '/', port: 80
-  };
-
-  var fixture = 'someFixture.json';
-  var fixtureLoc = nockWith.fixtures + '/' + fixture;
-
-  t.false(exists(fixtureLoc));
-
-  nockWith(fixture, function (done) {
-    http.request(options).end();
-    done();
-
-    t.true(exists(fixtureLoc));
-
-    fs.unlinkSync(fixtureLoc);
-    t.end();
   });
 
-});
+  nw.test('it records when configured correctly', function (t) {
+    nockWith.fixtures = __dirname + '/fixtures';
 
-//Adding this test because there was an issue when not calling
-//nock.activate() after calling nock.restore()
-tap.test('it can record twice', function (t) {
+    var options = {
+      host: 'www.google.com', method: 'GET', path: '/', port: 80
+    };
 
-  nockWith.fixtures = __dirname + '/fixtures';
+    var fixture = 'someFixture.json';
+    var fixtureLoc = nockWith.fixtures + '/' + fixture;
 
-  var options = {
-    host: 'www.google.com', method: 'GET', path: '/', port: 80
-  };
-  var fixture = 'someFixture2.json';
-  var fixtureLoc = nockWith.fixtures + '/' + fixture;
-  t.false(exists(fixtureLoc));
+    t.false(exists(fixtureLoc));
 
-  nockWith(fixture, function (done) {
-    http.request(options).end();
-    done();
+    nockWith(fixture, function (done) {
+      http.request(options).end();
+      done();
 
-    t.true(exists(fixtureLoc));
+      t.true(exists(fixtureLoc));
 
-    fs.unlinkSync(fixtureLoc);
-    t.end();
+      fs.unlinkSync(fixtureLoc);
+      t.end();
+    });
+
   });
 
-});
+  //Adding this test because there was an issue when not calling
+  //nock.activate() after calling nock.restore()
+  nw.test('it can record twice', function (t) {
+    nockWith.fixtures = __dirname + '/fixtures';
+
+    var options = {
+      host: 'www.google.com', method: 'GET', path: '/', port: 80
+    };
+    var fixture = 'someFixture2.json';
+    var fixtureLoc = nockWith.fixtures + '/' + fixture;
+    t.false(exists(fixtureLoc));
+
+    nockWith(fixture, function (done) {
+      http.request(options).end();
+      done();
+
+      t.true(exists(fixtureLoc));
+
+      fs.unlinkSync(fixtureLoc);
+      t.end();
+    });
+
+  });
 
 
-tap.test('it shouldn\'t allow outside calls', function (t) {
+  nw.test('it shouldn\'t allow outside calls', function (t) {
 
-  var fixture = 'wrongUri.json';
+    var fixture = 'wrongUri.json';
 
-  nockWith(fixture, function (done) {
+    nockWith(fixture, function (done) {
 
-    http.get('http://www.amazon.com', function(res) {
-      throw "should not request this";
-    }).on('error', function(err) {
-      t.equal(err.message, 'Nock: Not allow net connect for "www.amazon.com:80"');
+      http.get('http://www.amazon.com', function(res) {
+        throw "should not request this";
+      }).on('error', function(err) {
+        t.equal(err.message, 'Nock: Not allow net connect for "www.amazon.com:80"');
+        done();
+        t.end();
+      });
+
+    });
+
+  });
+
+
+  nw.test('it loads your recorded tests', function (t) {
+
+    nockWith('goodRequest.json', function (done) {
+      t.true(this.scopes.length > 0);
       done();
       t.end();
     });
 
   });
 
-});
+  nw.end();
+})
+.on('end', function () {
 
-
-tap.test('it loads your recorded tests', function (t) {
-
-  nockWith('goodRequest.json', function (done) {
-    t.true(this.scopes.length > 0);
-    done();
-    t.end();
-  });
+  nockWith.setMode('wild');
 
 });
-
