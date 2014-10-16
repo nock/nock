3fd2a62dc9c0671ff012386b73328a2f207de620
Keith Laban <kelaban17@gmail.com>
2014-10-15 21:51:18 -0400

    Added more tests, fixed some problems

--- tests/test_back.js
+++ tests/test_back.js
@@ -1,5 +1,6 @@
 
-var nockBack = require('../.').back
+var nock    = require('../.')
+  , nockBack= nock.back
   , tap     = require('tap')
   , http    = require('http')
   , fs      = require('fs')
@@ -7,22 +8,166 @@ var nockBack = require('../.').back
 
 var originalMode = nockBack.currentMode;
 
-tap.test('nockBack tests', function (nw) {
-  nockBack.setMode('record');
+function testNock (t) {
+  var dataCalled = false;
 
-  nw.test('nockBack throw an execption when fixtures is not set', function (t) {
+  var scope = nock('http://www.google.com')
+    .get('/')
+    .reply(200, "Hello World!");
 
-    try {
-      nockBack();
-    } catch (e) {
-      t.ok(true, 'excpected exception');
-      t.end();
-      return;
-    }
+  var req = http.request({
+      host: "www.google.com"
+    , path: '/'
+    , port: 80
+    }, function(res) {
 
-    t.fail(true, false, 'test should have ended');
+      t.equal(res.statusCode, 200);
+      res.on('end', function() {
+          t.ok(dataCalled);
+          scope.done();
+          t.end();
+        });
+      res.on('data', function(data) {
+          dataCalled = true;
+          t.ok(data instanceof Buffer, "data should be buffer");
+          t.equal(data.toString(), "Hello World!", "response should match");
+        });
+    });
 
+  req.end();
+}
+
+
+
+
+function nockBackWithFixture (t, scopesLoaded) {
+  var scopesLength = scopesLoaded ? 1 : 0;
+
+  nockBack('goodRequest.json', function (done) {
+    t.true(this.scopes.length === scopesLength);
+    http.get('http://www.google.com').end();
+    this.assertScopesFinished();
+    done();
+    t.end();
   });
+
+}
+
+
+
+
+tap.test('nockBack throw an execption when fixtures is not set', function (t) {
+
+  try {
+    nockBack();
+  } catch (e) {
+    t.ok(true, 'excpected exception');
+    t.end();
+    return;
+  }
+
+  t.fail(true, false, 'test should have ended');
+
+});
+
+
+
+
+tap.test('nockBack wild tests', function (nw) {
+  nockBack.fixtures = __dirname + '/fixtures';
+  nockBack.setMode('wild');
+
+  nw.test('normal nocks work', function (t) {
+    testNock(t);
+  });
+
+
+  nw.test('nock back doesn\'t do anything', function (t) {
+    nockBackWithFixture(t, false);
+  });
+
+})
+.on('end', function () {
+
+  nockBack.setMode(originalMode);
+
+});
+
+
+
+
+tap.test('nockBack dryrun tests', function (nw) {
+  nockBack.fixtures = __dirname + '/fixtures';
+  nockBack.setMode('dryrun');
+
+  nw.test('normal nocks work', function (t) {
+    testNock(t);
+  });
+
+
+  nw.test('uses recorded fixtures', function (t) {
+    nockBackWithFixture(t, true);
+  });
+
+  nw.test('goes it internet, doesn\'t recorded new fixtures', function (t) {
+    var dataCalled = false;
+
+    var fixture = 'someDryrunFixture.json';
+    var fixtureLoc = nockBack.fixtures + '/' + fixture;
+
+    t.false(exists(fixtureLoc));
+
+    nockBack(fixture, function (done) {
+      var req = http.request({
+          host: "www.amazon.com"
+        , path: '/'
+        , port: 80
+        }, function(res) {
+
+          t.equal(res.statusCode, 200);
+          res.on('end', function() {
+            var doneFails = false;
+
+            t.ok(dataCalled);
+            try {
+              done();
+              t.false(exists(fixtureLoc));
+              scope.done();
+            } catch(err) {
+              doneFails = true;
+            }
+            t.ok(doneFails);
+            t.end();
+          });
+
+          res.on('data', function(data) {
+            dataCalled = true;
+          });
+
+        });
+
+      req.on('error', function(err) {
+        if (err.code !== 'ECONNREFUSED') {
+          throw err;
+        }
+        t.end();
+      });
+
+      req.end();
+    });
+  });
+})
+.on('end', function () {
+
+  nockBack.setMode(originalMode);
+
+});
+
+
+
+
+tap.test('nockBack record tests', function (nw) {
+  nockBack.setMode('record');
 
   nw.test('it records when configured correctly', function (t) {
     nockBack.fixtures = __dirname + '/fixtures';
@@ -105,6 +250,46 @@ tap.test('nockBack tests', function (nw) {
   });
 
   nw.end();
+})
+.on('end', function () {
+
+  nockBack.setMode(originalMode);
+
+});
+
+
+
+
+tap.test('nockBack lockdown tests', function (nw) {
+  nockBack.fixtures = __dirname + '/fixtures';
+  nockBack.setMode('lockdown');
+
+  nw.test('normal nocks work', function (t) {
+    testNock(t);
+  });
+
+
+  nw.test('nock back loads scope', function (t) {
+    nockBackWithFixture(t, true);
+  });
+
+  nw.test('no unnocked http calls work', function (t) {
+    var req = http.request({
+        host: "google.com"
+      , path: '/'
+      }, function(res) {
+        throw new Error('should not come here!');
+      });
+
+
+    req.on('error', function (err) {
+      t.equal(err.message.trim(), 'Nock: Not allow net connect for "google.com:80"');
+      t.end();
+    });
+
+
+    req.end();
+  });
 })
 .on('end', function () {
 
