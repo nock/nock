b19bae1b4e68f2b3e45402b6ca794fd688ebaaa2
Alberto Pose <albertopose@gmail.com>
2014-10-29 11:10:39 -0300

    Fixes NOCK_OFF='true' not working for some cases.

---
+++ tests/test_nock_off.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var test          = require('tap').test;
+var mikealRequest = require('request');
+
+test('NOCK_OFF=true works for https', function(t) {
+  var original = process.env.NOCK_OFF;
+  process.env.NOCK_OFF = 'true';
+  var nock = require('../');
+  var scope = nock('https://www.google.com')
+  .get('/')
+  .reply(200, {foo: 'bar'});
+
+  var options = {
+    method: 'GET',
+    uri: 'https://www.google.com'
+  };
+
+  mikealRequest(options, function(err, resp, body) {
+    t.notOk(err);
+    t.notDeepEqual(body, '{"foo":"bar"}');
+    scope.done();
+    t.end();
+    process.env.NOCK_OFF = original;
+    return console.log(resp.statusCode, 'body length: ', body.length);
+  });
+});
+
+
