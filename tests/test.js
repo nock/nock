97b7f362c989c9732520d314dd536eecb70ee748
Justin <justincyork@gmail.com>
2015-03-20 21:14:33 +0000

    add coverage with istanbul and coveralls

---
+++ tests/test.js
@@ -0,0 +1,11 @@
+require('./test_common');
+require('./test_intercept');
+require('./test_redirects');
+require('./test_aws_dynamo');
+require('./test_back');
+require('./test_https_allowunmocked');
+require('./test_net_connect');
+require('./test_s3');
+require('./test_url_encoding');
+require('./test_nock_off');
+require('./test_recorder');
