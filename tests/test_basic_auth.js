16cfab46268bbc70a44a338122f17f8ecad09202
Rui Marinho <rpm@seegno.com>
2015-09-17 02:40:25 +0100

    Fix authentication using username only
    When using just an username for authentication, the password
    `undefined` would be used instead of `''`.

--- tests/test_basic_auth.js
+++ tests/test_basic_auth.js
@@ -1,10 +1,12 @@
 'use strict';
 
-var test = require('tap').test;
-var mikealRequest = require('request');
 var nock = require('../');
+var request = require('request');
+var test = require('tap').test;
 
-test("basic auth", function(t) {
+test('basic auth with username and password', function(t) {
+  t.plan(2);
+
   nock('http://super-secure.com')
     .get('/test')
     .basicAuth({
@@ -13,8 +15,8 @@ test("basic auth", function(t) {
     })
     .reply(200, 'Here is the content');
 
-  t.test('work when it match', function (t) {
-    mikealRequest({
+  t.test('succeeds when it matches', function (tt) {
+    request({
       url: 'http://super-secure.com/test',
       auth: {
         user: 'foo',
@@ -24,19 +26,54 @@ test("basic auth", function(t) {
       if (err) {
         throw err;
       }
-      t.equal(res.statusCode, 200);
-      t.equal(body, 'Here is the content');
-      t.end();
+      tt.equal(res.statusCode, 200);
+      tt.equal(body, 'Here is the content');
+      tt.end();
     });
   });
 
-  t.test('fail when it doesnt match', function (t) {
-    mikealRequest({
+  t.test('fails when it doesnt match', function (tt) {
+    request({
       url: 'http://super-secure.com/test',
     }, function(err, res, body) {
-      t.type(err, 'Error')
-      t.end();
+      tt.type(err, 'Error');
+      tt.end();
+    });
+  });
+});
+
+test('basic auth with username only', function(t) {
+  t.plan(2);
+
+  nock('http://super-secure.com')
+    .get('/test')
+    .basicAuth({
+      user: 'foo'
+    })
+    .reply(200, 'Here is the content');
+
+  t.test('succeeds when it matches', function (tt) {
+    request({
+      url: 'http://super-secure.com/test',
+      auth: {
+        user: 'foo'
+      }
+    }, function(err, res, body) {
+      if (err) {
+        throw err;
+      }
+      tt.equal(res.statusCode, 200);
+      tt.equal(body, 'Here is the content');
+      tt.end();
     });
   });
 
+  t.test('fails when it doesnt match', function (tt) {
+    request({
+      url: 'http://super-secure.com/test',
+    }, function(err, res, body) {
+      tt.type(err, 'Error');
+      tt.end();
+    });
+  });
 });
