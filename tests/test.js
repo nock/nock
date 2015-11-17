842e26107e9fdf70f72e7a5d1a3e901b10a088c2
Pedro Teixeira <i@pgte.me>
2015-11-17 09:40:00 +0000

    removed recorder tests for now. (see #404)

--- tests/test.js
+++ tests/test.js
@@ -12,7 +12,6 @@ require('./test_net_connect');
 require('./test_s3');
 require('./test_url_encoding');
 require('./test_nock_off');
-require('./test_recorder');
 require('./test_back_2');
 require('./test_data');
 require('./test_ipv6');
