95eb14faa05e5caaee8072fc9592c7e727434d61
Pedro Teixeira <i@pgte.me>
2016-01-08 15:23:27 +0000

    skip test if node 0.10

--- tests/test_content_encoding.js
+++ tests/test_content_encoding.js
@@ -3,24 +3,26 @@ var nock = require('../.');
 var http = require('http');
 var test = require('tap').test;
 
-test('accepts gzipped content', function(t) {
-  var message = 'Lorem ipsum dolor sit amet';
+if (zlib.gzipSync && zlib.gunzipSync) {
+  test('accepts gzipped content', function(t) {
+    var message = 'Lorem ipsum dolor sit amet';
 
-  var compressedMessage = zlib.gzipSync(message);
+    var compressedMessage = zlib.gzipSync(message);
 
-  nock('http://gziplandpartywoo')
-    .get('/foo')
-    .reply(200, compressedMessage, {
-      'X-Transfer-Length': String(compressedMessage.length),
-      'Content-Length': undefined,
-      'Content-Encoding': 'gzip',
-    });
+    nock('http://gziplandpartywoo')
+      .get('/foo')
+      .reply(200, compressedMessage, {
+        'X-Transfer-Length': String(compressedMessage.length),
+        'Content-Length': undefined,
+        'Content-Encoding': 'gzip',
+      });
 
-  http.get('http://gziplandpartywoo/foo', function(res) {
-    res.once('data', function(d) {
-      var dd = zlib.gunzipSync(d);
-      t.equal(dd.toString(), message);
-      res.once('end', t.end.bind(t));
+    http.get('http://gziplandpartywoo/foo', function(res) {
+      res.once('data', function(d) {
+        var dd = zlib.gunzipSync(d);
+        t.equal(dd.toString(), message);
+        res.once('end', t.end.bind(t));
+      });
     });
   });
-});
+}
