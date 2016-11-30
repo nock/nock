ef5b27ed3686582f1ac524d4119adaf06b3090d9
digitaltsai <digitaltsai@gmail.com>
2016-11-29 16:32:03 -0800

    Adds test for timeout

--- tests/test_timeout.js
+++ tests/test_timeout.js
@@ -2,20 +2,31 @@
 
 var nock    = require('../.');
 var test    = require('tap').test;
-var request = require('request');
+var http = require('http');
 
-test('something', function (t) {
+test('http request properly times out', function (t) {
     nock('http://google.com')
         .get('/')
         .delay(1000)
         .reply(200, {});
 
-    request({
-        url: 'http://google.com',
-        timeout: 10
-    }, function (err, response) {
-        t.ok(err);
-        t.equal(err.code, 'ETIMEDOUT')
+    var req = http.get({
+      host: 'google.com',
+      path: '/'
+    }, function(res) {
+      res.on('data', function(data) {
+        // need to listen to data to reach the end
+      });
+
+      res.on('end', function() {
+        t.fail('should have timed out before reaching here');
         t.end();
+      });
+    });
+
+    // https://nodejs.org/api/http.html#http_request_settimeout_timeout_callback
+    req.setTimeout(10, function() {
+      // this should be called before the response ends
+      t.end();
     });
 });
