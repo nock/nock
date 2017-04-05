7156fd6876a132481b9ee862a36e42aa4d0e6fb1
abarre <anthony.barre87@gmail.com>
2017-04-05 18:50:19 +0200

    [#863] Fix matchBody for object and array comparison

--- tests/test_body_match.js
+++ tests/test_body_match.js
@@ -5,64 +5,55 @@ var test = require('tap').test;
 var mikealRequest = require('request');
 var assert = require('assert');
 
-test('match body with regex', function (t) {
-
+var testBodyMatch = function (expected, actual, shouldMatch, t) {
   nock('http://encodingsareus.com')
-    .post('/', {auth: {passwd: /a.+/}})
+    .post('/', expected)
     .reply(200);
 
   mikealRequest({
     url: 'http://encodingsareus.com/',
     method: 'post',
-    json: {
-      auth: {
-        passwd: 'abc'
-      }
-    },
+    json: actual,
   }, function(err, res) {
-    if (err) throw err;
-    assert.equal(res.statusCode, 200);
-    t.end();
+    shouldMatch
+      ? assert.equal(res.statusCode, 200)
+      : assert(err && /No match for request/.test(err.message), 'nock should not intercept the request');
+      t.end();
+  });
+}
+
+
+test('body matching', function (t) {
+  t.test('match body with regex', function (t2) {
+    testBodyMatch({auth: {passwd: /a.+/}}, {auth: {passwd: 'abc'}}, true, t2);
   });
 
+  t.test('match body with regex inside array', function (t2) {
+    testBodyMatch({items: [{name: /t.+/}]}, {items: [{name: 'test'}]}, true, t2);
+  });
+
+  t.test('failing body matching with regex inside array', function (t2) {
+    testBodyMatch({items: [{name: /o.+/}]}, {items: [{name: 'test'}]}, false, t2);
+  });
+
+  t.test('failing body matching with different array length', function (t2) {
+    testBodyMatch({items: [1, 2]}, {items: [{items: [1, 2, 3]}]}, false, t2);
+  });
+
+  t.test('match body with empty object inside', function (t2) {
+    testBodyMatch({ obj: {}}, {obj: {}}, true, t2);
+  });
+
+  t.test('match body with identical objects', function (t2) {
+    testBodyMatch({ obj: {b:1, a:1}}, {obj: {b:1, a:1}}, true, t2);
+  });
+
+  t.test('failing body matching with different objects', function (t2) {
+    testBodyMatch({ obj: {a:1}}, {obj: {b:1, a:1}}, false, t2);
+  });
+
+  t.test('failing body matching with different nested objects', function (t2) {
+    testBodyMatch({ obj: {a: {b:1}}}, {obj: {a: {b:1, c:1}}}, false, t2);
+  });
+  t.end();
 });
-
-test('match body with regex inside array', function (t) {
-
-  nock('http://encodingsareus.com')
-    .post('/', {items: [{name: /t.+/}]})
-    .reply(200);
-
-  mikealRequest({
-    url: 'http://encodingsareus.com/',
-    method: 'post',
-    json: {
-      items: [{
-        name: 'test'
-      }]
-    },
-  }, function(err, res) {
-    if (err) throw err;
-    assert.equal(res.statusCode, 200);
-    t.end();
-  });
-})
-
-test('match body with empty object inside', function (t) {
-
-  nock('http://encodingsareus.com')
-    .post('/', { obj: {}})
-    .reply(200);
-
-  mikealRequest({
-    url: 'http://encodingsareus.com/',
-    method: 'post',
-    json: {
-      obj: {}
-    },
-  }, function(err, res) {
-    if (err) throw err;
-    assert.equal(res.statusCode, 200);
-    t.end();
-  });
-})
