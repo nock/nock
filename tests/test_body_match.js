1074a0a242ec9fad7acb51cccfb8726d8e589a39
Gavin Mogan <git@gavinmogan.com>
2017-10-23 13:18:19 -0700

    Re-add support for matching body to query string regex

--- tests/test_body_match.js
+++ tests/test_body_match.js
@@ -5,6 +5,27 @@ var test = require('tap').test;
 var mikealRequest = require('request');
 var assert = require('assert');
 
+test('match body is regex trying to match string', function (t) {
+
+  nock('http://encodingsareus.com')
+    .post('/', new RegExp("a.+"))
+    .reply(200);
+
+  mikealRequest({
+    url: 'http://encodingsareus.com/',
+    method: 'post',
+    json: {
+      auth: {
+        passwd: 'abc'
+      }
+    },
+  }, function(err, res) {
+    if (err) throw err;
+    assert.equal(res.statusCode, 200);
+    t.end();
+  });
+
+});
 test('match body with regex', function (t) {
 
   nock('http://encodingsareus.com')
