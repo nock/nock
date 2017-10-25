a748c8ef60d7a6062e46e77e15814301c5a4472e
Brian Lagoda <brian.lagoda@poncho.is>
2017-10-25 19:07:55 -0400

    adding a test per https://github.com/node-nock/nock/issues/976#issuecomment-331352387
    It sets a query that should always fail, but it breaks https matches anyway.
    
    I'm much less sure I can make a good fix.

--- tests/test_https_allowunmocked.js
+++ tests/test_https_allowunmocked.js
@@ -22,3 +22,24 @@ test('allowUnmocked for https', {skip: process.env.AIRPLANE}, function(t) {
     t.end();
   });
 });
+
+test('allowUnmocked for https with query test miss', {skip: process.env.AIRPLANE}, function(t) {
+  var nock = require('../');
+  nock.enableNetConnect();
+  nock('https://www.google.com', {allowUnmocked: true})
+  .get('/search')
+  .query(function() {return false;})
+  .reply(500);
+
+  var options = {
+    method: 'GET',
+    uri: 'https://www.google.com/search'
+  };
+
+  mikealRequest(options, function(err, resp, body) {
+    t.notOk(err, 'should be no error');
+    t.true(typeof body !== 'undefined', 'body should not be undefined');
+    t.true(body.length !== 0, 'body should not be empty');
+    t.end();
+  });
+});
