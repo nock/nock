fa58493dbe0d4fb7b8cd275ed3f1df55a150c775
n30n0v <n30n0v@gmail.com>
2017-11-25 22:17:58 +0300

    fix: set protocol correctly (#1027)
    fixes #976

--- tests/test_https_allowunmocked.js
+++ tests/test_https_allowunmocked.js
@@ -2,10 +2,11 @@
 
 var test          = require('tap').test;
 var mikealRequest = require('request');
+var nock          = require('../');
+
+nock.enableNetConnect();
 
 test('allowUnmocked for https', {skip: process.env.AIRPLANE}, function(t) {
-  var nock = require('../');
-  nock.enableNetConnect();
   nock('https://www.google.com/', {allowUnmocked: true})
   .get('/pathneverhit')
   .reply(200, {foo: 'bar'});
@@ -13,6 +14,26 @@ test('allowUnmocked for https', {skip: process.env.AIRPLANE}, function(t) {
   var options = {
     method: 'GET',
     uri: 'https://www.google.com'
+  };
+
+  mikealRequest(options, function(err, resp, body) {
+    t.notOk(err, 'should be no error');
+    t.true(typeof body !== 'undefined', 'body should not be undefined');
+    t.true(body.length !== 0, 'body should not be empty');
+    t.end();
+  });
+});
+
+test('allowUnmocked for https with query test miss', {skip: process.env.AIRPLANE}, function(t) {
+  nock.cleanAll();
+  nock('https://www.google.com', {allowUnmocked: true})
+    .get('/search')
+    .query(function() {return false;})
+    .reply(500);
+
+  var options = {
+    method: 'GET',
+    uri: 'https://www.google.com/search'
   };
 
   mikealRequest(options, function(err, resp, body) {
