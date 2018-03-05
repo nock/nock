b9329ed2f09e9171df772f47c64e817e503b382a
Mayank Badola <mbadola@expedia.com>
2018-03-05 10:42:17 +0530

    test: remove airplane dependency from "tests/test_back.js#L261"

--- tests/test_back.js
+++ tests/test_back.js
@@ -322,23 +322,44 @@ tap.test('nockBack record tests', function (nw) {
   //Adding this test because there was an issue when not calling
   //nock.activate() after calling nock.restore()
   nw.test('it can record twice', {skip: process.env.AIRPLANE}, function (t) {
+
+    t.plan(4)
+
     nockBack.fixtures = __dirname + '/fixtures';
 
-    var options = {
-      host: 'www.google.com', method: 'GET', path: '/', port: 80
-    };
-    var fixture = 'someFixture2.json';
-    var fixtureLoc = nockBack.fixtures + '/' + fixture;
+    const fixture = 'someFixture2.txt';
+    const fixtureLoc = nockBack.fixtures + '/' + fixture;
+
     t.false(exists(fixtureLoc));
 
     nockBack(fixture, function (done) {
-      http.request(options).end();
-      done();
+      const server = http.createServer((request, response) => {
+        t.pass('server received a request')
 
-      t.true(exists(fixtureLoc));
+        response.writeHead(200)
+        response.write('server served a response')
+        response.end()
+      })
 
-      fs.unlinkSync(fixtureLoc);
-      t.end();
+      server.listen(() => {
+        const request = http.request({
+          host: 'localhost',
+          path: '/',
+          port: server.address().port
+        }, (response) => {
+          done()
+
+          t.is(200, response.statusCode)
+          t.true(exists(fixtureLoc))
+
+          fs.unlinkSync(fixtureLoc)
+
+          server.close(t.end)
+        })
+
+        request.on('error', t.error)
+        request.end()
+      })
     });
 
   });
