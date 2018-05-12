944a8b87c1ce933e83e7e5438233c5c167b23225
Mayank Badola <mbadola@expedia.com>
2018-05-12 17:38:42 +0530

    Add localhost key and cert for https server mock

--- tests/test_intercept.js
+++ tests/test_intercept.js
@@ -26,6 +26,16 @@ globalCount = Object.keys(global).length;
 var acceptableLeaks = [
   '_key', '__core-js_shared__', 'fetch', 'Response', 'Headers', 'Request'];
 
+var sslServerOptions = {
+  key: fs.readFileSync(path.resolve(__dirname, './localhost.key')),
+  cert: fs.readFileSync(path.resolve(__dirname, './localhost.crt'))
+}
+
+const clientCA = {
+  ca: 'mocked',
+  rejectUnauthorized: false
+}
+
 test("double activation throws exception", function(t) {
   nock.restore();
   t.false(nock.isActive());
