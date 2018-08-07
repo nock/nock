50ab208b2b3e9787d76fb39e7b1a6f50ecda2795
Tim Rogers <me@timrogers.co.uk>
2018-08-07 17:54:42 +0100

    test: new SSL certificates for local testing (#1181)
    We use self-signed SSL certificates in our tests to check that
    Nock works okay with HTTPS.
    
    Unfortunately, these certificates expired on 2nd August 2018,
    making tests fail.
    
    This issues new ones, valid for 10 years from today (i.e.
    until 31st July 2028) and adds to `tests/ssl/index.js` the
    commands you need to run if you want to regenerate the needed
    files.

--- tests/ssl/index.js
+++ tests/ssl/index.js
@@ -1,5 +1,17 @@
 'use strict';
 
+// With OpenSSL installed, you can set up your CA and certificates with
+// the following commands, valid for 10 years:
+//
+//   openssl genrsa -out localhost.key 2048
+//   # Set the common name to "localhost"
+//   openssl req -new -key localhost.key -out localhost.csr
+//   openssl genrsa -out ca.key 2048
+//   # Set the common name to "Nock CA"
+//   openssl req -new -x509 -key ca.key -out ca.crt -days 3650
+//   openssl x509 -req -in localhost.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out localhost.crt -days 3650
+//   rm ca.srl localhost.csr
+//
 var https = require('https');
 var readFileSync = require('fs').readFileSync;
 var resolve = require('path').resolve;
