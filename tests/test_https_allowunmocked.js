903763ea1cc9ef4a64ccd58bee79f7a5a2dde720
Jonas Lilja <jonas@lilja.io>
2018-09-07 17:33:38 +0200

    Updated tests to use its own https server. query now also respects return value false.

--- tests/test_https_allowunmocked.js
+++ tests/test_https_allowunmocked.js
@@ -1,45 +1,68 @@
-'use strict';
+'use strict'
 
-var test          = require('tap').test;
-var mikealRequest = require('request');
-var nock          = require('../');
+const test = require('tap').test
+const fs = require('fs')
+const https = require('https')
+const nock = require('../')
+const request = require('request')
 
-nock.enableNetConnect();
+nock.enableNetConnect()
 
-test('allowUnmocked for https', {skip: process.env.AIRPLANE}, function(t) {
-  nock('https://www.google.com/', {allowUnmocked: true})
-  .get('/pathneverhit')
-  .reply(200, {foo: 'bar'});
+test('Nock with allowUnmocked and an url match', (test) => {
+  const options = {
+    key: fs.readFileSync('tests/ssl/ca.key'),
+    cert: fs.readFileSync('tests/ssl/ca.crt')
+  }
 
-  var options = {
-    method: 'GET',
-    uri: 'https://www.google.com'
-  };
+  const server = https.createServer(options, (req, res) => {
+    res.writeHead(200)
+    res.end({ status: 'default' })
+  })
 
-  mikealRequest(options, function(err, resp, body) {
-    t.notOk(err, 'should be no error');
-    t.true(typeof body !== 'undefined', 'body should not be undefined');
-    t.true(body.length !== 0, 'body should not be empty');
-    t.end();
-  });
-});
+  server.listen(() => {
+    nock(`https://127.0.0.1:${server.address().port}`, { allowUnmocked: true })
+      .get('/urlMatch')
+      .reply(201, { status: 'intercepted' })
 
-test('allowUnmocked for https with query test miss', {skip: process.env.AIRPLANE}, function(t) {
-  nock.cleanAll();
-  nock('https://www.google.com', {allowUnmocked: true})
-    .get('/search')
-    .query(function() {return false;})
-    .reply(500);
+    const url = `https://127.0.0.1:${server.address().port}/urlMatch`
 
-  var options = {
-    method: 'GET',
-    uri: 'https://www.google.com/search'
-  };
+    request(url, (error, { statusCode }, body) => {
+      test.notOk(error, 'Should be no error')
+      test.true(statusCode === 201, 'statusCode should match')
+      test.true(JSON.parse(body).status === 'intercepted', 'body should match')
+      test.end()
+    })
 
-  mikealRequest(options, function(err, resp, body) {
-    t.notOk(err, 'should be no error');
-    t.true(typeof body !== 'undefined', 'body should not be undefined');
-    t.true(body.length !== 0, 'body should not be empty');
-    t.end();
-  });
-});
+    server.close()
+  })
+})
+
+test('Nock with allowUnmocked, url match and query false', (test) => {
+  nock.cleanAll()
+
+  const options = {
+    key: fs.readFileSync('tests/ssl/ca.key'),
+    cert: fs.readFileSync('tests/ssl/ca.crt')
+  }
+
+  const server = https.createServer(options, (req, res) => {
+    res.writeHead(200)
+    res.end({ status: 'default' })
+  })
+
+  server.listen(() => {
+    nock(`https://127.0.0.1:${server.address().port}`, { allowUnmocked: true })
+      .get('/urlMatch')
+      .query(() => { return false })
+      .reply(201, { hello: 'there' })
+
+    request(`https://127.0.0.1:${server.address().port}/urlMatch`, (error, response, body) => {
+      test.true((error === null), 'should be no error')
+      test.true(typeof body !== 'undefined', 'body should not be undefined')
+      test.true(body.length !== 0, 'body should not be empty')
+      test.end()
+    })
+
+    server.close()
+  })
+})
