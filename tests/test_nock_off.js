db44b4d3ad79e6649a115e47e7276515d08975a8
Gregor <gregor@martynus.net>
2018-12-14 14:08:04 -0800

    test: remove airplaine mode from test/test_nock_off.js (#1077)

--- tests/test_nock_off.js
+++ tests/test_nock_off.js
@@ -1,27 +1,44 @@
 'use strict';
 
-var test          = require('tap').test;
+var test = require('tap').test;
 var mikealRequest = require('request');
 
+var ssl = require('./ssl')
+
 // Do not copy tests that rely on the process.env.AIRPLANE, we are deprecating that via #1231
-test('NOCK_OFF=true works for https', {skip: process.env.AIRPLANE}, function(t) {
+test('NOCK_OFF=true works for https', function (t) {
   var original = process.env.NOCK_OFF;
   process.env.NOCK_OFF = 'true';
   var nock = require('../');
-  var scope = nock('https://www.google.com')
-  .get('/')
-  .reply(200, {foo: 'bar'});
 
-  var options = {
-    method: 'GET',
-    uri: 'https://www.google.com'
-  };
+  t.plan(4);
 
-  mikealRequest(options, function(err, resp, body) {
-    t.notOk(err);
-    t.notDeepEqual(body, '{"foo":"bar"}');
-    scope.done();
-    process.env.NOCK_OFF = original;
-    t.end();
+  function middleware (request, response) {
+    t.pass('server received a request');
+    response.writeHead(200);
+    response.end('the real thing');
+  }
+
+  ssl.startServer(middleware, function (error, server) {
+    t.error(error);
+
+    var port = server.address().port
+    var scope = nock(`https://localhost:${port}`, { allowUnmocked: true})
+      .get('/')
+      .reply(200, 'mock')
+
+    var options = {
+      method: 'GET',
+      uri: `https://localhost:${port}`,
+      ca: ssl.ca
+    };
+
+    mikealRequest(options, function (err, resp, body) {
+      t.error(err);
+      t.equal(body, 'the real thing');
+      scope.done();
+      process.env.NOCK_OFF = original;
+      server.close(t.end);
+    });
   });
 });
