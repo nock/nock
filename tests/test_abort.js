23c45c226727532c429a8f5a4bebf406de920f99
Paul Melnikow <email@paulmelnikow.com>
2018-12-16 20:08:55 -0500

    test(test_abort): Improve style + readability

--- tests/test_abort.js
+++ tests/test_abort.js
@@ -1,71 +1,57 @@
 'use strict';
 
-var nock    = require('../.');
-var http    = require('http');
-var test    = require('tap').test;
+const nock = require('../.');
+const http = require('http');
+const test = require('tap').test;
 
-function assertEvents(t, cb) {
-  var gotAbort = false
-  var req = http.get('http://localhost:16829/status')
-    .once('abort', function () {
-      // Should trigger first
-      gotAbort = true
-    })
-    .once('error', function (err) {
-      // Should trigger last
-      t.equal(err.code, 'ECONNRESET')
-      t.ok(gotAbort, 'didn\'t get abort event');
-      t.end();
-      if(cb) {
-        cb();
-      }
-    });
-
-  process.nextTick(function(){
-    req.abort();
-  });
-}
-
-test('[actual] req.abort() should cause "abort" and "error" to be emitted', function (t) {
+test('[actual] req.abort() should cause "abort" and "error" to be emitted', t => {
   nock('http://localhost:16829')
     .get('/status')
     .delayConnection(500)
     .reply(204);
 
-  assertEvents(t);
+  let gotAbort = false
+  const req = http.get('http://localhost:16829/status')
+    .once('abort', () => {
+      // Should trigger first
+      gotAbort = true;
+    })
+    .once('error', err => {
+      // Should trigger last
+      t.equal(err.code, 'ECONNRESET');
+      t.ok(gotAbort, 'didn\'t get abort event');
+      t.end();
+    });
+  process.nextTick(() => req.abort());
 });
 
-test("abort is emitted before delay time", function(t) {
+test('abort is emitted before delay time', t => {
   nock('http://test.example.com')
-        .get('/status')
-        .delayConnection(500)
-        .reply(204);
+    .get('/status')
+    .delayConnection(500)
+    .reply(204);
 
-  var tstart = Date.now();
-  var req = http.get('http://test.example.com/status')
-  // Don't bother with the response
-  .once('abort', function() {
-    var actual = Date.now() - tstart;
-    t.ok(actual < 250, 'abort took only ' + actual + ' ms');
-    t.end();
-  })
-  .once('error', function(err) {
-    // don't care
-  });
+  const tstart = Date.now();
+  const req = http.get('http://test.example.com/status')
+    .once('abort', () => {
+      const actual = Date.now() - tstart;
+      t.ok(actual < 250, `abort took only ${actual} ms`);
+      t.end();
+    })
+    .once('error', () => {}) // Don't care.
+    // Don't bother with the response
 
-  setTimeout(function() {
-    req.abort();
-  }, 10);
+  setTimeout(() => req.abort(), 10);
 });
 
-test("Aborting an aborted request should not emit an error", function(t) {
+test("Aborting an aborted request should not emit an error", t => {
   nock('http://test.example.com')
     .get('/status')
     .reply(200);
 
-  var errorCount = 0
-  var req = http.get('http://test.example.com/status')
-    .on('error', function(err) {
+  let errorCount = 0
+  const req = http.get('http://test.example.com/status')
+    .on('error', err => {
       errorCount++;
       if (errorCount < 3) {
         // Abort 3 times at max, otherwise this would be an endless loop,
@@ -75,9 +61,9 @@ test("Aborting an aborted request should not emit an error", function(t) {
     });
   req.abort();
 
-  // Give nock some time to fail
-  setTimeout(function() {
-    t.equal(errorCount, 1,"Only one error should be sent.");
+  // Allow some time to fail.
+  setTimeout(() => {
+    t.equal(errorCount, 1, 'Only one error should be sent.');
     t.end();
-  },10);
+  }, 10);
 });
