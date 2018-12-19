9d96c7687047f1050802c87166750793a33bbb77
Paul Melnikow <github@paulmelnikow.com>
2018-12-18 19:25:25 -0500

    test(test_common): Rewrite in more direct style (#1306)

--- tests/test_common.js
+++ tests/test_common.js
@@ -1,74 +1,65 @@
 'use strict';
 
-var common  = require('../lib/common')
-  , tap     = require('tap')
-  , matchBody = require('../lib/match_body');
+const {test} = require('tap');
+const common = require('../lib/common');
+const matchBody = require('../lib/match_body');
 
-tap.test('matchBody ignores new line characters from strings', function(t) {
-  var str1 = "something //here is something more \n";
-  var str2 = "something //here is something more \n\r";
-  var matched = matchBody(str1, str2);
-  t.true(matched);
+test('matchBody ignores new line characters from strings', t => {
+  t.true(
+    matchBody(
+      "something //here is something more \n",
+      "something //here is something more \n\r"));
+  t.end();
+});
+
+test("when spec is a function, it's called with newline characters intact", t => {
+  const exampleBody = "something //here is something more \n";
+  let param
+  matchBody(body => { param = body }, exampleBody);
+  t.equal(param, exampleBody);
   t.end()
 });
 
-tap.test('matchBody keeps new line characters if specs is a function', function(t) {
-  var body = "something //here is something more \n";
-  var bodyAsSpecParameter = null
-  var spec = function(bodyToTest) {
-    bodyAsSpecParameter = bodyToTest
-  }
-  matchBody(spec, body);
-  t.equal(bodyAsSpecParameter, body);
+test('matchBody should not throw, when headers come node-fetch style as array', t => {
+  t.false(
+    matchBody.call(
+      {headers: {'Content-Type': ["multipart/form-data;"]}},
+      {},
+      "test"
+    ));
   t.end()
 });
 
-tap.test('matchBody should not throw, when headers come node-fetch style as array', function(t) {
-  var testThis = {
-    headers: {
-      'Content-Type': ["multipart/form-data;"]
-    }
-  }
-  matchBody.call(testThis, {}, "test");
+test("matchBody should not ignore new line characters from strings when Content-Type contains 'multipart'", t => {
+  t.true(
+    matchBody.call(
+      {headers: {'Content-Type': "multipart/form-data;"}},
+      "something //here is something more \nHello",
+      "something //here is something more \nHello"
+    ));
   t.end()
 });
 
-tap.test('matchBody should not ignore new line characters from strings when Content-Type contains \'multipart\'', function(t) {
-  var str1 = "something //here is something more \nHello";
-  var str2 = "something //here is something more \nHello";
-  var testThis = {
-    headers: {
-      'Content-Type': "multipart/form-data;"
-    }
-  }
-  var matched = matchBody.call(testThis, str1, str2);
-  t.true(matched);
+test("matchBody should not ignore new line characters from strings when Content-Type contains 'multipart' (arrays come node-fetch style as array)", t => {
+  t.true(
+    matchBody.call(
+      {headers: {'Content-Type': ["multipart/form-data;"]}},
+      "something //here is something more \nHello",
+      "something //here is something more \nHello"
+    ));
   t.end()
 });
 
-tap.test('matchBody should not ignore new line characters from strings when Content-Type contains \'multipart\' (arrays come node-fetch style as array)', function(t) {
-  var str1 = "something //here is something more \nHello";
-  var str2 = "something //here is something more \nHello";
-  var testThis = {
-    headers: {
-      'Content-Type': ["multipart/form-data;"]
-    }
-  }
-  var matched = matchBody.call(testThis, str1, str2);
-  t.true(matched);
+test('matchBody uses strict equality for deep comparisons', t => {
+  t.false(
+    matchBody(
+      { number: 1 },
+      '{"number": "1"}'
+    ));
   t.end()
 });
 
-tap.test('matchBody uses strict equality for deep comparisons', function(t) {
-  var spec = { number: 1 };
-  var body = '{"number": "1"}';
-  var matched = matchBody(spec, body);
-  t.false(matched);
-  t.end()
-});
-
-tap.test('isBinaryBuffer works', function(t) {
-
+test('isBinaryBuffer works', t => {
   //  Returns false for non-buffers.
   t.false(common.isBinaryBuffer());
   t.false(common.isBinaryBuffer(''));
@@ -80,86 +71,76 @@ tap.test('isBinaryBuffer works', function(t) {
   t.false(common.isBinaryBuffer(Buffer.from('8001', 'utf8')));
 
   t.end();
-
 });
 
-tap.test('headersFieldNamesToLowerCase works', function(t) {
-
-  var headers = {
-    'HoSt': 'example.com',
-    'Content-typE': 'plain/text'
-  };
-
-  var lowerCaseHeaders = common.headersFieldNamesToLowerCase(headers);
-
-  t.equal(headers.HoSt, lowerCaseHeaders.host);
-  t.equal(headers['Content-typE'], lowerCaseHeaders['content-type']);
-  t.end();
-
-});
-
-tap.test('headersFieldNamesToLowerCase throws on conflicting keys', function(t) {
-
-  var headers = {
-    'HoSt': 'example.com',
-    'HOST': 'example.com'
-  };
-
-  try {
-    common.headersFieldNamesToLowerCase(headers);
-  } catch(e) {
-    t.equal(e.toString(), 'Error: Failed to convert header keys to lower case due to field name conflict: host');
-    t.end();
-  }
-
-});
-
-tap.test('headersFieldsArrayToLowerCase works on arrays', function (t) {
-  var headers = ['HoSt', 'Content-typE'];
-
-  var lowerCaseHeaders = common.headersFieldsArrayToLowerCase(headers);
-
-  // Order doesn't matter.
-  lowerCaseHeaders.sort();
-
-  t.deepEqual(lowerCaseHeaders, ['content-type', 'host']);
+test('headersFieldNamesToLowerCase works', t => {
+  t.deepEqual(
+    common.headersFieldNamesToLowerCase({
+      HoSt: 'example.com',
+      'Content-typE': 'plain/text'
+    }),
+    {
+      host: 'example.com',
+      'content-type': 'plain/text'
+    });
   t.end();
 });
 
-tap.test('headersFieldsArrayToLowerCase deduplicates arrays', function (t) {
-  var headers = ['hosT', 'HoSt', 'Content-typE', 'conTenT-tYpe'];
-
-  var lowerCaseHeaders = common.headersFieldsArrayToLowerCase(headers);
-
-  // Order doesn't matter.
-  lowerCaseHeaders.sort();
-
-  t.deepEqual(lowerCaseHeaders, ['content-type', 'host']);
+test('headersFieldNamesToLowerCase throws on conflicting keys', t => {
+  t.throws(
+    () => common.headersFieldNamesToLowerCase({
+      'HoSt': 'example.com',
+      'HOST': 'example.com'
+    }),
+    {message: 'Failed to convert header keys to lower case due to field name conflict: host'}
+  );
   t.end();
 });
 
-tap.test('deleteHeadersField deletes fields with case-insensitive field names', function(t) {
+test('headersFieldsArrayToLowerCase works on arrays', function (t) {
+  t.deepEqual(
+    // Sort for comparison beause order doesn't matter.
+    common.headersFieldsArrayToLowerCase(['HoSt', 'Content-typE']).sort(),
+    ['content-type', 'host']
+    );
+  t.end();
+});
 
-  var headers = {
+test('headersFieldsArrayToLowerCase deduplicates arrays', function (t) {
+  t.deepEqual(
+    // Sort for comparison beause order doesn't matter.
+    common.headersFieldsArrayToLowerCase(['hosT', 'HoSt', 'Content-typE', 'conTenT-tYpe']).sort(),
+    ['content-type', 'host']
+    );
+  t.end();
+});
+
+test('deleteHeadersField deletes fields with case-insensitive field names', t => {
+  // Prepare.
+  const headers = {
     HoSt: 'example.com',
     'Content-typE': 'plain/text'
   };
 
+  // Confidence check.
   t.true(headers.HoSt);
   t.true(headers['Content-typE']);
 
+  // Act.
   common.deleteHeadersField(headers, 'HOST');
   common.deleteHeadersField(headers, 'CONTENT-TYPE');
 
+  // Assert.
   t.false(headers.HoSt);
   t.false(headers['Content-typE']);
-  t.end();
 
+  // Wrap up.
+  t.end();
 });
 
-tap.test('matchStringOrRegexp', function (t) {
+test('matchStringOrRegexp', function (t) {
   t.true(common.matchStringOrRegexp('to match', 'to match'), 'true if pattern is string and target matches');
-  t.false(common.matchStringOrRegexp('to match', 'not to match'), 'false if pattern is string and target doesn\'t match');
+  t.false(common.matchStringOrRegexp('to match', 'not to match'), "false if pattern is string and target doesn't match");
 
   t.true(common.matchStringOrRegexp(123, 123), 'true if pattern is number and target matches');
 
@@ -167,28 +148,23 @@ tap.test('matchStringOrRegexp', function (t) {
   t.false(common.matchStringOrRegexp(undefined, /not/), 'handle undefined target when pattern is regex');
 
   t.ok(common.matchStringOrRegexp('to match', /match/), 'match if pattern is regex and target matches');
-  t.false(common.matchStringOrRegexp('to match', /not/), 'false if pattern is regex and target doesn\'t match');
+  t.false(common.matchStringOrRegexp('to match', /not/), "false if pattern is regex and target doesn't match");
   t.end();
 });
 
-tap.test('stringifyRequest', function (t) {
-  var getMockOptions = function () {
-    return {
-      method: "POST",
-      port: 81,
-      proto: 'http',
-      hostname: 'www.example.com',
-      path: '/path/1',
-      headers: {
-        cookie: 'fiz=baz'
-      }
-    };
-  }
-  var body = {"foo": "bar"};
-  var postReqOptions = getMockOptions();
+test('stringifyRequest', function (t) {
+  const exampleOptions = {
+    method: "POST",
+    port: 81,
+    proto: 'http',
+    hostname: 'www.example.com',
+    path: '/path/1',
+    headers: {cookie: 'fiz=baz'}
+  };
 
-  t.equal(common.stringifyRequest(postReqOptions, body),
-    JSON.stringify({
+  t.deepEqual(
+    JSON.parse(common.stringifyRequest(exampleOptions, {foo: 'bar'})),
+    {
       "method":"POST",
       "url":"http://www.example.com:81/path/1",
       "headers":{
@@ -197,28 +173,31 @@ tap.test('stringifyRequest', function (t) {
       "body": {
         "foo": "bar"
       }
-    }, null, 2)
+    }
   );
 
-  var getReqOptions = getMockOptions();
-  getReqOptions.method = "GET";
-
-  t.equal(common.stringifyRequest(getReqOptions, null),
-    JSON.stringify({
+  t.deepEqual(
+    JSON.parse(common.stringifyRequest(
+      {
+        ...exampleOptions,
+        method: 'GET',
+      },
+      null)),
+    {
       "method":"GET",
       "url":"http://www.example.com:81/path/1",
       "headers":{
         "cookie": "fiz=baz"
       }
-    }, null, 2)
+    }
   );
 
   t.end();
 });
 
 
-tap.test('headersArrayToObject', function (t) {
-  var headers = [
+test('headersArrayToObject', function (t) {
+  const headers = [
     "Content-Type",
     "application/json; charset=utf-8",
     "Last-Modified",
@@ -233,7 +212,7 @@ tap.test('headersArrayToObject', function (t) {
     "expires": "fizbuzz"
   });
 
-  var headersMultipleSetCookies = headers.concat([
+  const headersMultipleSetCookies = headers.concat([
     "Set-Cookie",
     "foo=bar; Domain=.github.com; Path=/",
     "Set-Cookie",
