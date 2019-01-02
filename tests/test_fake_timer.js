184f79bfdc9e6232f1651272d8e2d97e328e542d
Chengxuan Xing <cxing@uk.ibm.com>
2019-01-02 20:39:33 +0000

    adding test for fake timer

---
+++ tests/test_fake_timer.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const nock = require('../.')
+const test = require('tap').test
+const request = require('request')
+const lolex = require('lolex')
+
+test('one function returns successfully when fake timer is enabled', function(t) {
+
+  let clock = lolex.install()
+  nock('http://www.google.com')
+    .get('/')
+    .reply(200)
+
+  request.get('http://www.google.com', function(err, resp) {
+    clock.uninstall()
+    if (err) {
+      throw err
+    }
+    t.equal(resp.statusCode, 200)
+    t.end()
+  })
+})
