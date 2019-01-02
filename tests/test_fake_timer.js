9ace2da6da945ee742b4299e70e6b57f518494f3
Chengxuan Xing <cxing@uk.ibm.com>
2019-01-02 20:42:54 +0000

    adding semicolons for repo style consistency

--- tests/test_fake_timer.js
+++ tests/test_fake_timer.js
@@ -1,23 +1,22 @@
 'use strict'
 
-const nock = require('../.')
-const test = require('tap').test
-const request = require('request')
-const lolex = require('lolex')
+const nock = require('../.');
+const test = require('tap').test;
+const request = require('request');
+const lolex = require('lolex');
 
 test('one function returns successfully when fake timer is enabled', function(t) {
-
-  let clock = lolex.install()
+  let clock = lolex.install();
   nock('http://www.google.com')
     .get('/')
-    .reply(200)
+    .reply(200);
 
   request.get('http://www.google.com', function(err, resp) {
-    clock.uninstall()
+    clock.uninstall();
     if (err) {
-      throw err
+      throw err;
     }
-    t.equal(resp.statusCode, 200)
-    t.end()
-  })
-})
+    t.equal(resp.statusCode, 200);
+    t.end();
+  });
+});
