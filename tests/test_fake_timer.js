5ee4ef3e0581c0f80ec56d1894b7c20c63443a2d
Chengxuan <xcxncu@gmail.com>
2019-01-03 06:05:50 +0000

    fix: Mock responses should fire when timers are mocked (#1336)
    Fix picked from #1335.
    
    Revert #1270. Fix #1334.

--- tests/test_fake_timer.js
+++ tests/test_fake_timer.js
@@ -1,23 +1,23 @@
-'use strict';
+'use strict'
 
-const nock = require('../.');
-const test = require('tap').test;
-const request = require('request');
-const lolex = require('lolex');
+const nock = require('../.')
+const test = require('tap').test
+const request = require('request')
+const lolex = require('lolex')
 
 // https://github.com/nock/nock/issues/1334
 test('one function returns successfully when fake timer is enabled', function(t) {
-  let clock = lolex.install();
+  const clock = lolex.install()
   nock('http://www.google.com')
     .get('/')
-    .reply(200);
+    .reply(200)
 
   request.get('http://www.google.com', function(err, resp) {
-    clock.uninstall();
+    clock.uninstall()
     if (err) {
-      throw err;
+      throw err
     }
-    t.equal(resp.statusCode, 200);
-    t.end();
-  });
-});
+    t.equal(resp.statusCode, 200)
+    t.end()
+  })
+})
