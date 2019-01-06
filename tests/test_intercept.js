7137d61b843a9ab8683157aba4205c717f387217
Paul Melnikow <email@paulmelnikow.com>
2019-01-05 21:11:31 -0500

    Run prettier

--- tests/test_intercept.js
+++ tests/test_intercept.js
@@ -1211,7 +1211,10 @@ test('response pipe without implicit end', t => {
         t.end()
       })
 
-      res.pipe(dest, { end: false })
+      res.pipe(
+        dest,
+        { end: false }
+      )
     }
   )
 })
