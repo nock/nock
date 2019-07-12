f5ee5ddcb1efc8fbb128e5a1a6eade61870fb291
Matt R. Wilson <mwilson@shutterstock.com>
2019-07-11 22:31:28 -0600

    fix: allow recording req headers when not outputting objects
    When exploring coverage gaps a few things were uncovered:
    - OutgoingMessage._headers has been deprecated in favor of `getHeaders()`. https://nodejs.org/api/deprecations.html#deprecations_dep0066_outgoingmessage_prototype_headers_outgoingmessage_prototype_headernames
    - IncommingMessage.rawHeaders is never falsy so there isn't a need to fall back to `headers`
    - There was a bug that kept request headers from being added to `matchHeader` lines in non-object outputs.
      `enable_reqheaders_recording` was only having an affect if `output_objects` was also true, but headers were never being added despite that because it was looking for headers under the wrong attribute on the request.
      A test was added to cover the flow of wanting the `matchHeader` as the other flow was already covered.

--- tests/test_recorder.js
+++ tests/test_recorder.js
@@ -587,7 +587,7 @@ test('records https correctly', t => {
   })
 })
 
-test('records request headers correctly', t => {
+test('records request headers correctly as an object', t => {
   const server = http.createServer((request, response) => response.end())
   t.once('end', () => server.close())
 
@@ -630,6 +630,37 @@ test('records request headers correctly', t => {
       )
       .end()
   })
+})
+
+test('records request headers correctly when not outputting objects', async t => {
+  t.plan(5)
+
+  nock.restore()
+  nock.recorder.clear()
+  t.equal(nock.recorder.play().length, 0)
+
+  const server = http.createServer((request, response) => {
+    t.pass('server received a request')
+    response.writeHead(200)
+    response.end()
+  })
+  t.once('end', () => server.close())
+  await new Promise(resolve => server.listen(resolve))
+  const { port } = server.address()
+
+  nock.recorder.rec({
+    dont_print: true,
+    enable_reqheaders_recording: true,
+  })
+
+  await got.post(`http://localhost:${port}`, { headers: { 'X-Foo': 'bar' } })
+
+  nock.restore()
+
+  const recorded = nock.recorder.play()
+  t.equal(recorded.length, 1)
+  t.type(recorded[0], 'string')
+  t.true(recorded[0].includes('  .matchHeader("x-foo", "bar")'))
 })
 
 test('records and replays gzipped nocks correctly', t => {
@@ -941,7 +972,7 @@ test('includes query parameters from superagent', t => {
     superagent
       .get(`http://localhost:${server.address().port}`)
       .query({ q: 'test search' })
-      .end(res => {
+      .end(() => {
         nock.restore()
         const ret = nock.recorder.play()
         t.true(ret.length >= 1)
@@ -951,7 +982,7 @@ test('includes query parameters from superagent', t => {
   })
 })
 
-test('encodes the query parameters when not outputing objects', t => {
+test('encodes the query parameters when not outputting objects', t => {
   const server = http.createServer((request, response) => {
     response.writeHead(200)
     response.end()
@@ -971,7 +1002,7 @@ test('encodes the query parameters when not outputing objects', t => {
     superagent
       .get(`http://localhost:${server.address().port}`)
       .query({ q: 'test search++' })
-      .end(res => {
+      .end(() => {
         nock.restore()
         const recording = nock.recorder.play()
         t.true(recording.length >= 1)
@@ -1217,7 +1248,7 @@ test('records and replays binary response correctly', t => {
     '47494638396101000100800000000000ffffff21f90401000000002c000000000100010000020144003b'
   const transparentGifBuffer = Buffer.from(transparentGifHex, 'hex')
 
-  // start server that always respondes with transparent gif at available port
+  // start server that always responds with transparent gif at available port
   const server = http.createServer((request, response) => {
     response.writeHead(201, {
       'Content-Type': 'image/gif',
@@ -1254,7 +1285,7 @@ test('records and replays binary response correctly', t => {
 
         const recordedFixtures = nock.recorder.play()
 
-        // stope server, stope recording, start intercepting
+        // stop server, stop recording, start intercepting
         server.close(error => {
           t.error(error)
 
