0b857f0f3e355e5d7193455dbaf9ba8e30a7ef91
Matt R. Wilson <mwilson@shutterstock.com>
2019-07-15 16:02:59 -0600

    feat(interceptor): duplicate query keys throw
    Resolves #1623
    
    BREAKING CHANGE: Providing a duplicate search parameter to the `query`
    method throws an error instead of ignoring subsequent values.

--- tests/test_query.js
+++ tests/test_query.js
@@ -121,17 +121,19 @@ test('query() accepts URLSearchParams as input', async t => {
   scope.done()
 })
 
-test('multiple set query keys use the first occurrence', async t => {
-  const scope = nock('http://example.test')
+test('query() throws for duplicate keys', async t => {
+  const interceptor = nock('http://example.test')
     .get('/')
     .query({ foo: 'bar' })
-    .query({ foo: 'baz' })
-    .reply()
 
-  const { statusCode } = await got('http://example.test?foo=bar')
-
-  t.is(statusCode, 200)
-  scope.done()
+  t.throws(
+    () => {
+      interceptor.query({ foo: 'baz' })
+    },
+    {
+      message: 'foo already defined as a query parameter',
+    }
+  )
 })
 
 test('query() matches a query string that contains special RFC3986 characters', t => {
