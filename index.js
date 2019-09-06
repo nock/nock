d6415d6e3f171521ead4c220fea08c00d558d723
Paul Melnikow <email@paulmelnikow.com>
2019-09-06 13:35:02 -0400

    refactor: Refactor exports to reduce indirection

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 'use strict'
 
+const back = require('./lib/back')
+const emitter = require('./lib/global_emitter')
+const {
+  activate,
+  isActive,
+  isDone,
+  pendingMocks,
+  activeMocks,
+  removeInterceptor,
+  disableNetConnect,
+  enableNetConnect,
+  removeAll,
+} = require('./lib/intercept')
 const recorder = require('./lib/recorder')
-module.exports = require('./lib/scope')
+const { Scope, load, loadDefs, define } = require('./lib/scope')
 
-module.exports.recorder = {
-  rec: recorder.record,
-  clear: recorder.clear,
-  play: recorder.outputs,
-}
+module.exports = (basePath, options) => new Scope(basePath, options)
 
-module.exports.back = require('./lib/back')
-module.exports.restore = recorder.restore
+Object.assign(module.exports, {
+  activate,
+  isActive,
+  isDone,
+  pendingMocks,
+  activeMocks,
+  removeInterceptor,
+  disableNetConnect,
+  enableNetConnect,
+  // TODO-12.x Historically `nock.cleanAll()` has returned the nock global.
+  // The other global methods do noto do this, so it's not clear this was
+  // deliberate or is even helpful. This shim is included for backward
+  // compatibility and shoulud be replaced with an alias to `removeAll()`.
+  cleanAll() {
+    removeAll()
+    return module.exports
+  },
+  load,
+  loadDefs,
+  define,
+  emitter,
+  recorder: {
+    rec: recorder.record,
+    clear: recorder.clear,
+    play: recorder.outputs,
+  },
+  restore: recorder.restore,
+  back,
+})
