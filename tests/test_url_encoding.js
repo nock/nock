f756e803f8ba04fff899ccba0d823ebff69ffed2
Paul Melnikow <email@paulmelnikow.com>
2019-10-26 11:57:51 -0400

    refactor: chai assertions for test_url_encoding
    Ref https://github.com/nock/nock/issues/1305#issuecomment-544878116

--- tests/test_url_encoding.js
+++ tests/test_url_encoding.js
@@ -1,19 +1,19 @@
 'use strict'
 
 const { test } = require('tap')
-const mikealRequest = require('request')
+const { expect } = require('chai')
 const nock = require('..')
+const got = require('./got_client')
 
 require('./cleanup_after_each')()
 
-test('url encoding', t => {
-  nock('http://example.test')
+test('url encoding', async t => {
+  const scope = nock('http://example.test')
     .get('/key?a=[1]')
     .reply(200)
 
-  mikealRequest('http://example.test/key?a=[1]', function(err, res) {
-    if (err) throw err
-    t.equal(res.statusCode, 200)
-    t.end()
-  })
+  const { statusCode } = await got('http://example.test/key?a=[1]')
+  expect(statusCode).to.equal(200)
+
+  scope.done()
 })
