59f66679ab4d0d690a6b8f297af194430f4a1397
Matt R. Wilson <mwilson@shutterstock.com>
2020-02-20 21:31:16 -0800

    refactor(test): Chai assert & Mocha DSL querystring encoding

--- tests/test_encode_querystring.js
+++ tests/test_encode_querystring.js
@@ -1,29 +1,23 @@
 'use strict'
 
-const request = require('request')
-const { test } = require('tap')
+const { expect } = require('chai')
 const nock = require('..')
+const got = require('./got_client')
 
-require('./cleanup_after_each')()
+require('./setup')
 
-test('encode query string', t => {
+it('encode query string', async () => {
   const query1 = { q: '(nodejs)' }
 
-  nock('https://example.test')
+  const scope = nock('https://example.test')
     .get('/test')
     .query(query1)
     .reply(200, 'success')
 
-  request(
-    {
-      url: 'https://example.test/test',
-      qs: query1,
-      method: 'GET',
-    },
-    function(error, response, body) {
-      t.ok(!error)
-      t.deepEqual(body, 'success')
-      t.end()
-    }
-  )
+  const { statusCode, body } = await got('https://example.test/test?q=(nodejs)')
+
+  expect(statusCode).to.equal(200)
+  expect(body).to.equal('success')
+
+  scope.done()
 })
