afbd3438464083e6feb76ede2a87c4db8da9cdac
Matt R. Wilson <github.public@mattw.co>
2020-03-03 11:45:34 -0700

    refactor(test): Mocha DSL for destroy & dynamic mocks. (#1941)

--- tests/test_destroy.js
+++ tests/test_destroy.js
@@ -2,42 +2,42 @@
 
 const { expect } = require('chai')
 const http = require('http')
-const { test } = require('tap')
 const nock = require('..')
 
-require('./cleanup_after_each')()
 require('./setup')
 
-test('req.destroy should emit error event if called with error', t => {
-  nock('http://example.test')
-    .get('/')
-    .reply(404)
+describe('`res.destroy()`', () => {
+  it('should emit error event if called with error', done => {
+    nock('http://example.test')
+      .get('/')
+      .reply(404)
 
-  const respErr = new Error('Response error')
+    const respErr = new Error('Response error')
 
-  http
-    .get('http://example.test/', res => {
-      expect(res.statusCode).to.equal(404)
-      res.destroy(respErr)
-    })
-    .once('error', err => {
-      expect(err).to.equal(respErr)
-      t.end()
-    })
-})
+    http
+      .get('http://example.test/', res => {
+        expect(res.statusCode).to.equal(404)
+        res.destroy(respErr)
+      })
+      .once('error', err => {
+        expect(err).to.equal(respErr)
+        done()
+      })
+  })
 
-test('req.destroy should not emit error event if called without error', t => {
-  nock('http://example.test')
-    .get('/')
-    .reply(403)
+  it('should not emit error event if called without error', done => {
+    nock('http://example.test')
+      .get('/')
+      .reply(403)
 
-  http
-    .get('http://example.test/', res => {
-      expect(res.statusCode).to.equal(403)
-      res.destroy()
-      t.end()
-    })
-    .once('error', () => {
-      expect.fail('should not emit error')
-    })
+    http
+      .get('http://example.test/', res => {
+        expect(res.statusCode).to.equal(403)
+        res.destroy()
+        done()
+      })
+      .once('error', () => {
+        expect.fail('should not emit error')
+      })
+  })
 })
