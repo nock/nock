d682351240a2092442d22a60e7418626b1208e4b
Motin <motin@motin.eu>
2020-06-04 15:28:16 +0300

    fix: Gzipped request bodies also work when header is passed as an array (#2009)

--- tests/test_gzip_request.js
+++ tests/test_gzip_request.js
@@ -36,6 +36,35 @@ it('should accept and decode gzip encoded application/json', done => {
   req.end()
 })
 
+it('should accept and decode gzip encoded application/json, when headers come node-fetch style as array', done => {
+  const message = {
+    my: 'contents',
+  }
+
+  nock('http://example.test')
+    .post('/')
+    .reply(function (url, actual) {
+      expect(actual).to.deep.equal(message)
+      done()
+      return [200]
+    })
+
+  const req = http.request({
+    hostname: 'example.test',
+    path: '/',
+    method: 'POST',
+    headers: {
+      'content-encoding': ['gzip'],
+      'content-type': ['application/json'],
+    },
+  })
+
+  const compressedMessage = zlib.gzipSync(JSON.stringify(message))
+
+  req.write(compressedMessage)
+  req.end()
+})
+
 it('should accept and decode deflate encoded application/json', done => {
   const message = {
     my: 'contents',
