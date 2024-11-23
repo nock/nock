00f6340728c1230aebccfafd95f2d17fdc3fb5d5
Michael Solomon <micheal540@gmail.com>
2024-11-24 01:49:39 +0200

    fix

--- tests/test_fetch.js
+++ tests/test_fetch.js
@@ -550,5 +550,43 @@ describe('Native Fetch', () => {
 
       nocks.forEach(nock => nock.done())
     })
+
+    it('records and replays deflated nocks correctly', async () => {
+      const exampleText = '<html><body>example</body></html>'
+
+      const { origin } = await startHttpServer((request, response) => {
+        const body = zlib.deflateSync(exampleText)
+
+        response.writeHead(200, { 'content-encoding': 'deflate' })
+        response.end(body)
+      })
+
+      nock.restore()
+      nock.recorder.clear()
+      expect(nock.recorder.play()).to.be.empty()
+
+      nock.recorder.rec({
+        dont_print: true,
+        output_objects: true,
+      })
+
+      const response1 = await fetch(origin)
+      expect(await response1.text()).to.equal(exampleText)
+      expect(response1.headers.get('content-encoding')).to.equal('deflate')
+
+      nock.restore()
+      const recorded = nock.recorder.play()
+      nock.recorder.clear()
+      nock.activate()
+
+      expect(recorded).to.have.lengthOf(1)
+      const nocks = nock.define(recorded)
+
+      const response2 = await fetch(origin)
+      expect(await response2.text()).to.equal(exampleText)
+      expect(response1.headers.get('content-encoding')).to.equal('deflate')
+
+      nocks.forEach(nock => nock.done())
+    })
   })
 })
